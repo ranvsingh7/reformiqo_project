cc420f64c28d0c7eabdb047c53e05008703cc3d5
Ranveer <ranvsingh7@gmail.com>
2022-12-13 10:26:29 +0530

    calander added

--- src/components/Customers/Customer.jsx
+++ src/components/Customers/Customer.jsx
@@ -1,27 +1,40 @@
 import React from "react";
 
-
-import Page_heading from "../Page_Heading/Page_heading"
+import Page_heading from "../Page_Heading/Page_heading";
 import { useState, useEffect, useRef } from "react";
 import "./Customers.scss";
-import { Table, Checkbox, Affix } from "antd";
-import { Resizable } from 'react-resizable';
+import { Table, Checkbox, Affix, DatePicker } from "antd";
+import { Resizable } from "react-resizable";
 import { CSVLink } from "react-csv";
 import jsPDF from "jspdf";
 import "jspdf-autotable";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DateRangePicker } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; 
+import { addDays } from 'date-fns';
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
 
 import { CgSearch } from "react-icons/cg";
 import { BiExport } from "react-icons/bi";
 import { BiFilter } from "react-icons/bi";
 import { render } from "@testing-library/react";
 import ReactToPrint from "react-to-print";
-
+const { RangePicker } = DatePicker;
 const Customer = () => {
   const [exportOpen, setExportOpen] = useState(false);
   const [settingOpen, setSettingOpen] = useState(false);
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [dateRange, setDateRange] = useState([
+    {
+      startDate: new Date(),
+      endDate: addDays(new Date(), 7),
+      key: 'selection'
+    }
+  ]);
 
-  const menuRef = useRef(null)
+  const menuRef = useRef(null);
 
   const dataSource = [
     {
@@ -34,7 +47,6 @@ const Customer = () => {
       email: "parth@gmail.com",
       phone: 9988998899,
       address: "Reformiqo Business Service Pvt. Ltd.",
-      
     },
     {
       key: "2",
@@ -45,7 +57,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "3",
@@ -56,7 +68,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "4",
@@ -67,7 +79,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "5",
@@ -78,7 +90,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "6",
@@ -89,7 +101,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "7",
@@ -100,7 +112,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "8",
@@ -111,7 +123,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "9",
@@ -122,7 +134,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "10",
@@ -133,7 +145,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "11",
@@ -144,7 +156,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "12",
@@ -155,7 +167,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "13",
@@ -166,7 +178,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "14",
@@ -177,7 +189,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "15",
@@ -188,7 +200,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
     {
       key: "16",
@@ -199,7 +211,7 @@ const Customer = () => {
       type_category: "Wholeseler",
       email: "parth@gmail.com",
       phone: 9988998899,
-      address: "Reformiqo Business Service Pvt. Ltd."
+      address: "Reformiqo Business Service Pvt. Ltd.",
     },
 
     // ...
@@ -211,9 +223,9 @@ const Customer = () => {
       dataIndex: "business_name",
       key: "business_name",
       resizable: true,
-      fixed:'left',
-      align:'left',
-      width:270,
+      fixed: "left",
+      align: "left",
+      width: 270,
     },
     {
       title: "Type",
@@ -221,8 +233,8 @@ const Customer = () => {
       dataIndex: "type",
       key: "type",
       resizable: true,
-      width:100,
-      align:'left'
+      width: 100,
+      align: "left",
     },
     {
       title: "Account",
@@ -230,8 +242,8 @@ const Customer = () => {
       dataIndex: "account",
       key: "account",
       resizable: true,
-      width:170,
-      align:'left'
+      width: 170,
+      align: "left",
     },
     {
       title: "Unadjusted Account",
@@ -239,8 +251,8 @@ const Customer = () => {
       dataIndex: "unadjusted_account",
       key: "unadjusted_account",
       resizable: true,
-      width:180,
-      align:'left'
+      width: 180,
+      align: "left",
     },
     {
       title: "Type Category",
@@ -248,8 +260,8 @@ const Customer = () => {
       dataIndex: "type_category",
       key: "type_category",
       resizable: true,
-      width:150,
-      align:'left'
+      width: 150,
+      align: "left",
     },
     {
       title: "Email",
@@ -257,14 +269,13 @@ const Customer = () => {
       dataIndex: "email",
       key: "email",
       resizable: true,
-      width:150,
-      align:'left'
+      width: 150,
+      align: "left",
     },
-    
   ];
   const [columns, setColumns] = useState(columnsData);
-  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-  const [selectedRows, setSelectedRows] = useState([]);
+ 
+  
 
   const csvLink = {
     filename: "customer_data.csv",
@@ -289,50 +300,41 @@ const Customer = () => {
     setSettingOpen(!settingOpen);
   };
 
-
-  // useEffect(() => {
-  //   let handler = (e) => {
-  //     if (e.target) {
-  //       setExportOpen(false);
-  //     }
-  //     // setSettingOpen(false);
-  //   };
-  //   document.addEventListener("mousedown", handler);
-  // });
+ 
   useEffect(() => {
-
-    let handleClickOutside = (event)=> {
+    let handleClickOutside = (event) => {
       if (!menuRef.current.contains(event.target)) {
         // Close the dropdown list
         setSettingOpen(false);
         setExportOpen(false);
       }
-    }
-    document.addEventListener('mousedown', handleClickOutside);
+    };
+    document.addEventListener("mousedown", handleClickOutside);
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   });
 
   const handleDragEnd = (e) => {
-    if ( e.destination.index===0) {
-      return
-    }
-    else{
+    if (e.destination.index === 0) {
+      return;
+    } else {
       let tempData = Array.from(columns);
-    let [source_data] = tempData.splice(e.source.index, 1);
-    tempData.splice(e.destination.index, 0, source_data);
-    setColumns(tempData);
+      let [source_data] = tempData.splice(e.source.index, 1);
+      tempData.splice(e.destination.index, 0, source_data);
+      setColumns(tempData);
     }
-    console.log(e.destination.index)
+    console.log(e.destination.index);
   };
 
-  const componentRef=useRef()
-
+  const componentRef = useRef();
 
   return (
     <div className="customers">
-      <Page_heading parent={"Business Account"} child={window.location.pathname.slice(1)}/>
+      <Page_heading
+        parent={"Business Account"}
+        child={window.location.pathname.slice(1)}
+      />
       <div className="table_nav">
         <div className="search_customer">
           <div className="search_icon">
@@ -341,8 +343,8 @@ const Customer = () => {
           <input type="text" placeholder="Search Customer" />
         </div>
 
-        <div className="tableBtn_container"ref={menuRef} >
-          <div className="tableBtn export"  onClick={openExport}>
+        <div className="tableBtn_container" ref={menuRef}>
+          <div className="tableBtn export" onClick={openExport}>
             <div className="btn_icon">
               <BiExport size={15} />
             </div>
@@ -358,9 +360,9 @@ const Customer = () => {
                   <img src="./images/icons/print_icon.svg" alt="print_icon" />
                 </div>
                 {/* <span>Print</span> */}
-                <ReactToPrint 
-                  trigger={()=> <span>Print</span>}
-                  content={()=> componentRef.current}
+                <ReactToPrint
+                  trigger={() => <span>Print</span>}
+                  content={() => componentRef.current}
                 />
               </div>
               <CSVLink {...csvLink} className="csvLink">
@@ -404,7 +406,6 @@ const Customer = () => {
               src="./images/icons/setting.svg"
               alt="icon"
               onClick={openSetting}
-              
             />
             <DragDropContext onDragEnd={handleDragEnd}>
               <div
@@ -418,10 +419,12 @@ const Customer = () => {
                     <div ref={provider.innerRef} {...provider.droppableProps}>
                       {columns.map((item, index) => (
                         <Draggable
-                        draggableId={item.key}
-                        index={index}
-                        key={item.key}
-                        isDragDisabled={item.title==="Business Name" ? true:false}
+                          draggableId={item.key}
+                          index={index}
+                          key={item.key}
+                          isDragDisabled={
+                            item.title === "Business Name" ? true : false
+                          }
                         >
                           {(provider) => (
                             <div
@@ -454,27 +457,38 @@ const Customer = () => {
         </div>
       </div>
 
-      <div className="tableData" >
-        {/* <Resizable> */}
-          <Table
-          ref={componentRef}
-            rowSelection={{
-              type: "checkbox",
-              columnTitle: "",
-              selectedRowKeys,
-              onChange: (selectedRowKeys, selectedRows) => {
-                setSelectedRowKeys(selectedRowKeys);
-                setSelectedRows(selectedRows);
-              },
-              checkboxStyle:{color:'red'}
+    <DateRangePicker onChange={item => setDateRange([item.selection])}
+  showSelectionPreview={true}
+  moveRangeOnFirstSelection={false}
+  months={2}
+  ranges={dateRange}
+  direction="horizontal"/>
+  <RangePicker />
 
-            }}
-            dataSource={dataSource}
-            columns={columns}
-            scroll={{y:400, x:1120}}
-            style={{maxWidth:1120}}
-            rowClassName={(record)=> record.key%2 === 0 ? 'highlight_row':''}
-          />
+
+
+      <div className="tableData">
+        {/* <Resizable> */}
+        <Table
+          ref={componentRef}
+          rowSelection={{
+            type: "checkbox",
+            columnTitle: "",
+            selectedRowKeys,
+            onChange: (selectedRowKeys, selectedRows) => {
+              setSelectedRowKeys(selectedRowKeys);
+              setSelectedRows(selectedRows);
+            },
+            checkboxStyle: { color: "red" },
+          }}
+          dataSource={dataSource}
+          columns={columns}
+          scroll={{ y: 400, x: 1120 }}
+          style={{ maxWidth: 1133 }}
+          rowClassName={(record) =>
+            record.key % 2 === 0 ? "highlight_row" : ""
+          }
+        />
         {/* </Resizable> */}
       </div>
     </div>
